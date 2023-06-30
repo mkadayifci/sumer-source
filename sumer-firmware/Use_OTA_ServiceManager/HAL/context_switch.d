5c58b5794e3530d069c6b94b40deabe4c9674eb0
Mehmet Kadayıfçı <mkadayifci@gmail.com>
2023-06-30 19:48:09 +0300

    Delete sumer-firmware/Use_OTA_ServiceManager directory

--- sumer-firmware/Use_OTA_ServiceManager/HAL/context_switch.d
+++
@@ -1,5 +0,0 @@
-HAL/context_switch.o: \
- /Users/mkadayifci/Documents/BlueNRG-1_2\ DK\ 3.2.3/Library/hal/src/context_switch.s \
- /Users/mkadayifci/Documents/BlueNRG-1_2\ DK\ 3.2.3/Library/hal/src/../inc/asm.h
-
-/Users/mkadayifci/Documents/BlueNRG-1_2\ DK\ 3.2.3/Library/hal/src/../inc/asm.h:
--- sumer-firmware/Use_OTA_ServiceManager/HAL/osal.d
+++
@@ -1,5 +0,0 @@
-HAL/osal.o: \
- /Users/mkadayifci/Documents/BlueNRG-1_2\ DK\ 3.2.3/Library/hal/src/osal.c \
- ../../../../../../Library/hal/inc/osal.h
-
-../../../../../../Library/hal/inc/osal.h:
